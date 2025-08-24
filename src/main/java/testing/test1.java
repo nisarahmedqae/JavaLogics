6d5f80d116546ee5286effffeb2cc5feebd07102
Nisar Ahmed <nisarahmed.qae@gmail.com>
2025-08-24 18:07:11 +0530

    24 aug c4

--- src/main/java/testing/test1.java
+++ src/main/java/testing/test1.java
@@ -2,37 +2,8 @@ package testing;
 
 public class test1 {
 
-        // check given number is Armstrong number/ Narcissistic number
-        // pluperfect digital invariant (PPDI) number/ plus perfect number
-
-        public static boolean isArmStrongNumber(int num) {
-            if (num < 0) {
-                return false;
-            }
-
-            int power = String.valueOf(num).length();
-            int sum = 0;
-
-            for(int ele: String.valueOf(num).toCharArray()){
-                int factor = 1;
-                for (int i = 0; i < power; i++) {
-                    factor = factor * Character.getNumericValue(ele);
-                }
-                sum = sum + factor;
-            }
-
-            if (sum == num) {
-                return true;
-            }
-            return false;
-        }
-
-        public static void main(String[] args) {
-            System.out.println(isArmStrongNumber(0)); // true
-            System.out.println(isArmStrongNumber(-1));
-            System.out.println(isArmStrongNumber(153)); // true
-            System.out.println(isArmStrongNumber(1674));
-            System.out.println(isArmStrongNumber(12));
-        }
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 
 }
